275945c329173bffcd3ff7c07f7ef3256fbb8ac5
peter future <peter_future@outlook.com>
2014-06-28 13:28:03 +0800

    rename app to dttv

--- SimplePlayer/gen/com/example/simpleplayer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.simpleplayer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ dttv/jni/vo_android.cpp
@@ -0,0 +1,28 @@
+//render image with android
+
+#include <jni.h>
+#include <string.h>
+#include <android/log.h>
+
+extern "C"{
+
+#include "dtvideo_android.h"
+
+}
+
+namespace android {
+
+extern "C"{
+
+
+
+
+
+
+
+
+
+
+}
+
+}

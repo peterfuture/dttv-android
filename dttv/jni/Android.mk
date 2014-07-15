0770537804f10a2ace849ccf15a81d22d2d8985e
peter future <peter_future@outlook.com>
2014-07-15 10:12:43 +0800

    update dtp lib linked

--- dttv/jni/Android.mk
+++ dttv/jni/Android.mk
@@ -1,12 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
+##############################################
 include $(CLEAR_VARS)
+LOCAL_MODULE := dtp_prebuilt
+LOCAL_SRC_FILES := $(LOCAL_PATH)/libs/libdtp.so
+include $(PREBUILT_SHARED_LIBRARY)
 
+##############################################
+include $(CLEAR_VARS)
 LOCAL_MODULE    := dtp_jni
 LOCAL_SRC_FILES := register_natives.c dtp_native.c render_android.c ao_android.cpp vo_android.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_LDLIBS    := -llog -L$(LOCAL_PATH)/libs -ldtp -lffmpeg -lz
+LOCAL_LDLIBS    := -llog -lz
 
 #android env
 LOCAL_C_INCLUDES += $(AOSP_TREE)/frameworks/av/include/       #media
@@ -14,6 +20,6 @@ LOCAL_C_INCLUDES += $(AOSP_TREE)/system/core/include          #cutils
 LOCAL_C_INCLUDES += $(AOSP_TREE)/hardware/libhardware/include #hardware
 LOCAL_C_INCLUDES += $(AOSP_TREE)/frameworks/native/include    #utils
 
-LOCAL_LDLIBS     += -L$(AOSP_OUT)/system/lib -lmedia  -lutils -lGLESv1_CM
+LOCAL_LDLIBS     += -L$(AOSP_OUT)/system/lib -lmedia  -lutils -lGLESv1_CM -L$(LOCAL_PATH)/libs -ldtp
 
 include $(BUILD_SHARED_LIBRARY)
--- dttv/jni/include/dtplayer_api.h
+++ dttv/jni/include/dtplayer_api.h
@@ -57,14 +57,105 @@ typedef struct dtplayer_para
     int (*update_cb) (player_state_t * sta);
 } dtplayer_para_t;
 
+/* 
+ * DTPLAYER API DEFINITION
+ *
+ * */
+
+/* 
+ * do global initialization of dtplayer.
+ * finish format detect, stream-demuxer init, etc
+ *
+ * @param para - parameter setup by caller
+ * @return handle - pointer to dtplayer handle which used for do more contrl works
+ *
+ * */
 void *dtplayer_init (dtplayer_para_t * para);
-int dtplayer_start (void *player_priv);
-int dtplayer_pause (void *player_priv);
-int dtplayer_resume (void *player_priv);
-int dtplayer_stop (void *player_priv);
-int dtplayer_seek (void *player_priv, int s_time); // seek to cur_time + s_time
-int dtplayer_seekto (void *player_priv, int s_time); // seek to s_time
-int dtplayer_get_mediainfo (void *player_priv, dt_media_info_t *info);
-int dtplayer_get_states (void *player_priv, player_state_t * state);
+
+/*
+ * mediainfo:
+ * get all media information using this api
+ * need to be called after dtplayer_init
+ *
+ * @param handle - dtplayer handle
+ * @param info - mediainfo structure, store
+ * @return ret - 0 success , negtive failed
+ * 
+ * */
+int dtplayer_get_mediainfo (void *handle, dt_media_info_t *info);
+
+/*
+ * start playing:
+ * data fill loop 
+ * decode-render process
+ *
+ * @param handle - dtplayer handle
+ * @return ret - 0 success , negtive failed
+ * 
+ * */
+int dtplayer_start (void *handle);
+
+/*
+ * pause player:
+ * pause render thread, reset part will block
+ *
+ * @param handle - dtplayer handle
+ * @return ret - 0 success , negtive failed
+ * 
+ * */
+int dtplayer_pause (void *handle);
+
+/*
+ * resume player:
+ * resume render thread
+ *
+ * @param handle - dtplayer handle
+ * @return ret - 0 success , negtive failed
+ * 
+ * */
+int dtplayer_resume (void *handle);
+
+/*
+ * stop player:
+ * quit stream-demuxer-decoder-render module
+ *
+ * @param handle - dtplayer handle
+ * @return ret - 0 success , negtive failed
+ * 
+ * */
+int dtplayer_stop (void *handle);
+
+/*
+ * seek:
+ * seek to (cur_time + step), not recommended, will remove later
+ *
+ * @param handle - dtplayer handle
+ * @param step - step
+ * @return ret - 0 success , negtive failed
+ * 
+ * */
+int dtplayer_seek (void *handle, int step);
+
+/*
+ * seekto:
+ * seek to dest_pos, recommended API
+ *
+ * @param handle - dtplayer handle
+ * @param dest_pos - dest position time (s)
+ * @return ret - 0 success , negtive failed
+ * 
+ * */
+int dtplayer_seekto (void *handle, int dest_pos);
+
+/*
+ * dtplayer states update:
+ * called by user, get dtplayer states, for more details refer to player_state_t definition
+ *
+ * @param handle - dtplayer handle
+ * @param state 
+ * @return ret - 0 success , negtive failed
+ * 
+ * */
+int dtplayer_get_states (void *handle, player_state_t * state);
 
 #endif

27344dac0fe6a5c5e72f20d8d2ba131edff96f20
peter future <peter_future@outlook.com>
2014-08-04 22:04:01 +0800

    fix audiotrack crash bug

--- dttv/jni/ao_android.cpp
+++ dttv/jni/ao_android.cpp
@@ -56,8 +56,15 @@ static int android_audio_init(dtaudio_para_t *para)
 {
 	int ret = 0;
 	status_t status;
+    sp<AudioTrack> track;
+    int session_id;
+    
+    uint32_t afSampleRate;
+    size_t afFrameCount;
+    uint32_t frameCount;
+    int bufferCount = 4;
+
 	memcpy(&wrapper->para,para,sizeof(dtaudio_para_t));
-	AudioTrack *track;
 	android_ao_ctx_t *ctx = (android_ao_ctx_t *)malloc(sizeof(*ctx));
 	if(!ctx)
 	{
@@ -71,35 +78,39 @@ static int android_audio_init(dtaudio_para_t *para)
 		goto FAIL;
 	}
 	wrapper->ao_priv = ctx;
-	ctx->sptrack = new AudioTrack();
-	track = ctx->sptrack.get();
-	if (track  == NULL) {
-		__android_log_print(ANDROID_LOG_DEBUG,TAG,"AudioTrack Create Failed!");
-		ret = -1;
-		goto FAIL;
-	}
-
-	status = track->set(AUDIO_STREAM_MUSIC,
-						para->dst_samplerate,
-						AUDIO_FORMAT_PCM_16_BIT,
-						(para->dst_channels == 1) ? AUDIO_CHANNEL_OUT_MONO : AUDIO_CHANNEL_OUT_STEREO,
-						0,       // frameCount
-						AUDIO_OUTPUT_FLAG_NONE, // flags
-						audioCallback,
-						wrapper, // user when callback
-						0,       // notificationFrames
-						0,       // shared buffer
-						false,
-						0);
-
-	if (status != NO_ERROR) {
-		__android_log_print(ANDROID_LOG_DEBUG,TAG,"track->set returns %d", status);
-		track->stop();
-		ctx->sptrack.clear();
-		ret = -1;
-		goto FAIL;
-	}
+    
+    if (AudioSystem::getOutputFrameCount(&afFrameCount, AUDIO_STREAM_MUSIC) != NO_ERROR) {
+        return NO_INIT;
+    }
+    if (AudioSystem::getOutputSamplingRate(&afSampleRate, AUDIO_STREAM_MUSIC) != NO_ERROR) {
+        return NO_INIT;
+    }
+    frameCount = (para->dst_samplerate*afFrameCount*bufferCount)/afSampleRate;
+    
+    session_id = AudioSystem::newAudioSessionId();
+    track = new AudioTrack(
+                AUDIO_STREAM_MUSIC,
+                para->dst_samplerate,
+                AUDIO_FORMAT_PCM_16_BIT,
+                (para->dst_channels == 1) ? AUDIO_CHANNEL_OUT_MONO : AUDIO_CHANNEL_OUT_STEREO,
+                frameCount, // frameCount
+                AUDIO_OUTPUT_FLAG_NONE,
+                audioCallback,
+                wrapper,
+                0, // notification frames
+                session_id, // mSessionID
+                AudioTrack::TRANSFER_CALLBACK,
+                NULL, // offload info
+                0  // mUID 
+            );
+    
+    if(track == 0 || track->initCheck() != NO_ERROR)
+    {
+        __android_log_print(ANDROID_LOG_DEBUG,TAG,"Android Audiotrack new failed \n");
+        goto FAIL;
+    }
 	track->start();
+    ctx->sptrack = track;
 	__android_log_print(ANDROID_LOG_DEBUG,TAG,"Android AO Init OK\n");
 	return 0;
 FAIL:
@@ -114,13 +125,9 @@ static int android_audio_pause()
 	__android_log_print(ANDROID_LOG_DEBUG,TAG,"android out pause");
 
 	android_ao_ctx_t *ctx = (android_ao_ctx_t *)wrapper->ao_priv;
-	AudioTrack *track = ctx->sptrack.get();
-	if (!track) {
-		__android_log_print(ANDROID_LOG_DEBUG,TAG,"No track instance!\n");
-		return -1;
-	}
-	track->pause();
-	return 0;
+	if(ctx->sptrack != 0)
+        ctx->sptrack->pause();
+   	return 0;
 }
 
 static int android_audio_resume()
@@ -128,12 +135,8 @@ static int android_audio_resume()
 	__android_log_print(ANDROID_LOG_DEBUG,TAG,"android out pause");
 
 	android_ao_ctx_t *ctx = (android_ao_ctx_t *)wrapper->ao_priv;
-	AudioTrack *track = ctx->sptrack.get();
-	if (!track) {
-		__android_log_print(ANDROID_LOG_DEBUG,TAG,"No track instance!\n");
-		return -1;
-	}
-	track->start();
+	if(ctx->sptrack != 0)
+        ctx->sptrack->start();
 	return 0;
 }
 
@@ -152,16 +155,20 @@ static int android_audio_level()
 static int64_t android_audio_latency()
 {
 	android_ao_ctx_t *ctx = (android_ao_ctx_t *)wrapper->ao_priv;
-	AudioTrack *track = ctx->sptrack.get();
+    if(ctx->sptrack == 0)
+        return 0;
 
+    return 0;
+#if 0
 	int level = buf_level(&ctx->dbt);
 	unsigned int sample_num;
 	uint64_t latency;
 	float pts_ratio = 0.0;
 	pts_ratio = (double) 90000 / wrapper->para.dst_samplerate;
 	sample_num = level / (wrapper->para.dst_channels * wrapper->para.bps / 8);
-	latency = (sample_num * pts_ratio) + (int64_t)track->latency();
+	latency = (sample_num * pts_ratio) + (int64_t)ctx->sptrack->latency();
 	return latency;
+#endif
 }
 
 static int android_audio_stop()
@@ -169,9 +176,11 @@ static int android_audio_stop()
 	if(wrapper->ao_priv)
 	{
 		android_ao_ctx_t *ctx = (android_ao_ctx_t *)wrapper->ao_priv;
-		AudioTrack *track = ctx->sptrack.get();
-		track->stop();
-		ctx->sptrack.clear();
+		if(ctx->sptrack != 0)
+        {
+            ctx->sptrack->stop();
+            ctx->sptrack.clear();
+        }
 		buf_release(&ctx->dbt);
 		free(ctx);
 		wrapper->ao_priv = NULL;

6dc8e7a346815f4eb7ea6f74b687b7656f8e2aa2
peter future <peter_future@outlook.com>
2014-08-19 15:36:45 +0800

    add opengl impl

--- dttv/src/dttv/app/VideoPlayerActivity.java
+++ dttv/src/dttv/app/VideoPlayerActivity.java
@@ -4,13 +4,19 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.microedition.khronos.opengles.GL10;
+
 import android.app.Activity;
+import dttv.app.widget.GlVideoView;
 import android.content.Intent;
+import android.opengl.EGLConfig;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
@@ -22,6 +28,7 @@ import dttv.app.DtPlayer.OnPreparedListener;
 import dttv.app.utils.Constant;
 import dttv.app.utils.Log;
 import dttv.app.utils.TimesUtil;
+import dttv.app.widget.GlVideoView;
 /**
  * VideoPlayer Activity
  * 
@@ -38,6 +45,7 @@ public class VideoPlayerActivity extends Activity implements OnClickListener{
 	private TextView currentTimeTxt,totalTimeTxt;
 	private ImageButton preBtn,nextBtn,pauseBtn;
 	private SeekBar playerProgressBar;
+	private GlVideoView glSurfaceView;
 	private long[] ids;
 	private int seek_flag = 0;
 	@Override
@@ -86,6 +94,12 @@ public class VideoPlayerActivity extends Activity implements OnClickListener{
 		pauseBtn = (ImageButton)mBarView.findViewById(R.id.dt_play_pause_btn);
 		nextBtn = (ImageButton)mBarView.findViewById(R.id.dt_play_next_btn);
 		playerProgressBar = (SeekBar)mBarView.findViewById(R.id.dt_play_progress_seekbar);
+		
+		//opengl
+		glSurfaceView = (GlVideoView)findViewById(R.id.glvideo_view);
+		glSurfaceView.setRenderer(new GLSurfaceViewRender());
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+        //glSurfaceView.setOnTouchListener((OnTouchListener) this);
 	}
 	
 	private void initListener(){
@@ -261,4 +275,39 @@ public class VideoPlayerActivity extends Activity implements OnClickListener{
 		//dtPlayer.release();
 		super.onDestroy();
 	}
+		
+	//---------------------------OPENGL------------------------------//
+	
+	class GLSurfaceViewRender implements GLSurfaceView.Renderer {  
+		public void onSurfaceCreated(GL10 gl, EGLConfig config) {  
+            Log.i(TAG, "onSurfaceCreated");
+            // 设置背景颜色 - load backgroud picture 
+            //gl.glClearColor(0.0f, 0f, 1f, 0.5f);
+        }  
+  
+        @Override  
+        public void onSurfaceChanged(GL10 gl, int width, int height) {  
+            //other case
+        }
+  
+        @Override  
+        public void onDrawFrame(GL10 gl) {  
+            //Log.i(TAG, "onDrawFrame");  
+            // 清除屏幕和深度缓存(如果不调用该代码, 将不显示glClearColor设置的颜色)  
+            // 同样如果将该代码放到 onSurfaceCreated 中屏幕会一直闪动  
+            //gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        	Log.i(TAG, "draw enter");
+        }
+
+		@Override
+		public void onSurfaceCreated(GL10 arg0,
+				javax.microedition.khronos.egl.EGLConfig arg1) {
+			// TODO Auto-generated method stub
+			
+		}  
+  
+    }  
+	
+	//-----------------------------OPENGL----------------------------//
+	
 }

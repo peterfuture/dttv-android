46680e549ecf7019f0766a8a1b00dc068564e89f
shihx1 <shihx1@shihx1.(none)>
2014-09-10 19:55:56 +0800

    add screen config

--- dttv/src/dttv/app/widget/GlVideoView.java
+++ dttv/src/dttv/app/widget/GlVideoView.java
@@ -1,6 +1,13 @@
 package dttv.app.widget;
 
+import dttv.app.R;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 
@@ -11,5 +18,19 @@ public class GlVideoView extends GLSurfaceView {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+		/*Paint paint = new Paint();
+		canvas.drawText("画圆=========：", 10, 20, paint);// 画文本
+		canvas.restore();
+		canvas.drawCircle(100, 150, 50, paint);
+		canvas.restore();
+		Drawable drawable = getResources().getDrawable(R.drawable.ic_launcher);
+		BitmapDrawable  bitmapDrawable = (BitmapDrawable)drawable;
+		Bitmap bitmap = bitmapDrawable.getBitmap();
+		canvas.drawBitmap(bitmap, 100, 300, paint);*/
+	}
 }

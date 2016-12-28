9fe1789bc42d898ecfb127e20293376b82b9b7da
peter-s <peter_future@outlook.com>
2016-12-28 18:56:18 +0800

    update dtplayer headers

--- dttv/3rd/libdtp/include/dt_log.h
+++ dttv/3rd/libdtp/include/dt_log.h
@@ -3,15 +3,8 @@
 
 #if ENABLE_ANDROID
 #include <android/log.h>
-
-#define dt_info(p1,...)    __android_log_print(ANDROID_LOG_INFO,p1,__VA_ARGS__)
-#define dt_error(p1,...)   __android_log_print(ANDROID_LOG_INFO,p1,__VA_ARGS__)
-#define dt_debug(p1,...)   //__android_log_print(ANDROID_LOG_INFO,p1,__VA_ARGS__)
-#define dt_warning(p1,...) //__android_log_print(ANDROID_LOG_INFO,p1,__VA_ARGS__)
-
 #endif
 
-#if ENABLE_LINUX
 typedef enum {
     DT_LOG_INVALID = -1,
     DT_LOG_DEBUG,
@@ -29,6 +22,5 @@ void dt_info(void *tag, const char *fmt, ...);  // default info level
 
 void dt_set_log_level(int level);
 void dt_get_log_level(int level);
-#endif
 
 #endif

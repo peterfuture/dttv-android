538ff4910ca061af7134f7fcafdd78da08a78b79
peter-s <peter_future@outlook.com>
2017-06-11 19:51:48 +0800

    [test] update dttv testcode

--- dttv/dttvlib/src/androidTest/java/app/dttv/dttvlib/ExampleInstrumentedTest.java
+++ dttv/dttvlib/src/androidTest/java/app/dttv/dttvlib/ExampleInstrumentedTest.java
@@ -1,26 +1,10 @@
 package app.dttv.dttvlib;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 /**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Created by peter on 17-6-11.
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+public class ExampleInstrumentedTest extends TestCase {
 
-        assertEquals("app.dttv.dttvlib.test", appContext.getPackageName());
-    }
 }
--- dttv/dttvlib/src/test/java/app/dttv/dttvlib/ExampleUnitTest.java
+++ dttv/dttvlib/src/test/java/app/dttv/dttvlib/ExampleUnitTest.java
@@ -1,17 +1,10 @@
 package app.dttv.dttvlib;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Created by peter on 17-6-11.
  */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
+public class ExampleUnitTest extends TestCase {
+
 }
